d55bac7993779796610e7ef330c3ccc2e1f9ac02
sandeepbikkana <sandeepbikkana@gmail.com>
2025-08-04 13:55:55 +0530

    updated

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+<<<<<<< HEAD
 app.get('/', (req, res) => res.send('CI/CD Node.js App Working! good.'));
+=======
+app.get('/', (req, res) => res.send('CI/CD Node.js App Working! Test page'));
+>>>>>>> d19e87b (updated)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
